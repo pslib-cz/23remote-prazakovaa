29f47ada0dae07ed2c5529f622ae1374d814d3ae
nella <147390743+prazakovaa@users.noreply.github.com>
2024-06-14 10:59:23 +0200

    autonom-final

--- main.ts
+++ main.ts
@@ -17,23 +17,12 @@ enum ServoDirection {
     Center = 1,
     Right = 0
 }
+
 const allIRPins: Array<number> = [Pins.wr, Pins.wl, Pins.r, Pins.l, Pins.c, Pins.trig]
 for (let pin of allIRPins) {
     pins.setPull(pin, PinPullMode.PullNone);
 }
 
-const carMotor = (leftwheel: number = 0, rightwheel: number = 0): void => {
-    if (leftwheel === 0 && rightwheel === 0) { PCAmotor.MotorStopAll(); return; }
-
-    //PCAmotor.MotorRun(PCAmotor.Motors.M1, -1 * abrakadabra)
-    //PCAmotor.MotorRun(PCAmotor.Motors.M4, -1 * abrakabadra)
-}
-
-const servoMove = (direction: ServoDirection): void => {
-    PCAmotor.GeekServo(PCAmotor.Servos.S1, 500 + 1000 * direction)
-    basic.pause(2000)
-    PCAmotor.StopServo(PCAmotor.Servos.S1)
-}
 
 type Protokol = {
     x: number;
@@ -72,7 +61,7 @@ logoo = "0"
         stop: input.isGesture(Gesture.ScreenDown)
     }
 
-basic.pause(350)
+basic.pause(100)
 encode(proto)
 
 /* OVLADANI
